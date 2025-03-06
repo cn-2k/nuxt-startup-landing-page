3af9d7003a7cb53dc5d4aa2f4775ed5d96672d64
cn-2k <caio.vinnicius2k@gmail.com>
2025-03-06 14:31:57 -0300

    feat: added darkmode to rising stars

--- app/components/RisingStars.vue
+++ app/components/RisingStars.vue
@@ -3,15 +3,15 @@
     class="rising-stars absolute inset-0 overflow-hidden w-screen h-[45vh] bg-gradient-to-b from-20% to-80%"
   >
     <div
-      class="rising-stars__small animate-risingstar [animation-duration:333s] w-px h-px bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-px after:h-px after:bg-transparent"
+      class="rising-stars__small animate-risingstar text-black dark:text-white [animation-duration:333s] w-px h-px bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-px after:h-px after:bg-transparent"
       :style="`box-shadow: ${smallStars.join(',')}`"
     />
     <div
-      class="rising-stars__medium animate-risingstar [animation-duration:666s] w-[2px] h-[2px] bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-[2px] after:h-[2px] after:bg-transparent"
+      class="rising-stars__medium animate-risingstar text-black dark:text-white [animation-duration:666s] w-[2px] h-[2px] bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-[2px] after:h-[2px] after:bg-transparent"
       :style="`box-shadow: ${mediumStars.join(',')}`"
     />
     <div
-      class="rising-stars__large animate-risingstar [animation-duration:999s] w-[3px] h-[3px] bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-[3px] after:h-[3px] after:bg-transparent"
+      class="rising-stars__large animate-risingstar text-black dark:text-white [animation-duration:999s] w-[3px] h-[3px] bg-transparent after:content-[''] after:absolute after:top-[2048px] after:w-[3px] after:h-[3px] after:bg-transparent"
       :style="`box-shadow: ${largeStars.join(',')}`"
     />
   </div>
@@ -33,22 +33,22 @@ const random = (max: number) => Math.floor(Math.random() * max)
 
 const smallStars = computed(() => {
   return Array.from({ length: size.value }, () => {
-    return `${random(width.value)}px ${random(height.value)}px #FFF, ${random(
+    return `${random(width.value)}px ${random(height.value)}px , ${random(
       width.value
-    )}px ${random(height.value)}px #FFF`
+    )}px ${random(height.value)}px`
   })
 })
 
 const mediumStars = Array.from({ length: size.value / 2 }, () => {
-  return `${random(width.value)}px ${random(height.value)}px #FFF, ${random(
+  return `${random(width.value)}px ${random(height.value)}px, ${random(
     width.value
-  )}px ${random(height.value)}px #FFF`
+  )}px ${random(height.value)}px`
 })
 
 const largeStars = Array.from({ length: size.value / 4 }, () => {
-  return `${random(width.value)}px ${random(height.value)}px #FFF, ${random(
+  return `${random(width.value)}px ${random(height.value)}px, ${random(
     width.value
-  )}px ${random(height.value)}px #FFF`
+  )}px ${random(height.value)}px`
 })
 </script>
 
